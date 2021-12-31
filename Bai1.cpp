025c21f31b4ff5a450a0e35cf6de89d03b0c9a88
RealaxER <95581165+RealaxER@users.noreply.github.com>
2021-12-31 10:29:29 +0700

    Create Bai1.cpp

---
+++ Bai1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cout << "Nhap gia tri cua a: ";
+	cin >> a;
+	
+	cout << "Nhap gia tri cua b: ";
+	cin >> b;
+	
+	if(a>b)
+	{
+		cout << "gia tri a la lon nhat ";
+	}
+	
+	else
+	{
+		cout << "gia tri b la lon nhat";
+	}
+	
+	return 0;
+}
